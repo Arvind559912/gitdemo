b63a4dd58f34892451ac9b477506bc72217ccdd0
Arvind <arvindnitian95@gmail.com>
2020-03-05 10:42:03 +0530

    practicspgm

---
+++ practicspgm.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int a;
+long b;
+char c;
+float d;
+double e;
+int main() {
+// Complete the code.
+cin>>a>>b>>c>>d>>e;
+cout<<a<<endl;
+cout<<b<<endl;
+cout<<c<<endl;
+cout<<d<<endl;
+cout<<e<<endl;
+return 0;
+}
